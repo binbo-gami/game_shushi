5a27e3d187046faf62dc19beb745a81b1e9b2bf2
binbo-gami <mintwodao@gmail.com>
2025-08-02 07:10:12 +0900

    Update settings.py

--- game_shushi/settings.py
+++ game_shushi/settings.py
@@ -93,3 +93,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CSRF_TRUSTED_ORIGINS = ['https://dice-management-tool.onrender.com']
